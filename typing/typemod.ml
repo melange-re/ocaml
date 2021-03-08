ostera/bs-ocaml

[compiler] add check_exists to avoid unnecessary writing cmi files

typing/typemod.ml
@@ -1777,7 +1777,7 @@ let () =
 
 (* Typecheck an implementation file *)
 
-let type_implementation_more sourcefile outputprefix modulename initial_env ast =
+let type_implementation_more ?check_exists sourcefile outputprefix modulename initial_env ast =
   Cmt_format.clear ();
   try
   Typecore.reset_delayed_checks ();
@@ -1832,7 +1832,7 @@ let type_implementation_more sourcefile outputprefix modulename initial_env ast 
       if not !Clflags.dont_write_files then begin
         let deprecated = Builtin_attributes.deprecated_of_str ast in
         let cmi =
-          Env.save_signature ~deprecated
+          Env.save_signature ?check_exists ~deprecated
             simple_sg modulename (outputprefix ^ ".cmi")
         in
         Cmt_format.save_cmt  (outputprefix ^ ".cmt") modulename

melange-re/ocaml

typing/typemod.ml
@@ -2621,7 +2621,7 @@ let gen_annot outputprefix sourcefile annots =
   Cmt2annot.gen_annot (Some (outputprefix ^ ".annot"))
     ~sourcefile:(Some sourcefile) ~use_summaries:false annots
 
-let type_implementation_more sourcefile outputprefix modulename initial_env ast =
+let type_implementation_more ?check_exists sourcefile outputprefix modulename initial_env ast =
   Cmt_format.clear ();
   Misc.try_finally (fun () ->
       Typecore.reset_delayed_checks ();
@@ -2683,7 +2683,7 @@ let type_implementation_more sourcefile outputprefix modulename initial_env ast 
           if not !Clflags.dont_write_files then begin
             let alerts = Builtin_attributes.alerts_of_str ast in
             let cmi =
-              Env.save_signature ~alerts
+              Env.save_signature ?check_exists ~alerts
                 simple_sg modulename (outputprefix ^ ".cmi")
             in
             let annots = Cmt_format.Implementation str in